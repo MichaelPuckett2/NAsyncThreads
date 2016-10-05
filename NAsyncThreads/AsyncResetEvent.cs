492aa010b6379023b4b2841536b06adca8e0e050
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2016-10-05 12:47:46 -0400

    AsyncResetEvent class added and code to show and test it.

---
+++ NAsyncThreads/AsyncResetEvent.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace NAsyncThreads
+{
+    public class AsyncResetEvent
+    {
+        private volatile int threadCounter = 0;
+        private readonly int MaxSimultaneousThreads;
+        private readonly int MaxSimultaneousThreadsIgnoreTimeout;
+        private static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
+        /// <summary>
+        /// Constructs AsyncResetEvent with the MaxSimultaneousActions allowed.
+        /// </summary>
+        /// <param name="maxSimultaneousActions">The number of actions allowed to be invoked simultaneously. A value less than 1 is defaulted to 1.</param>
+        public AsyncResetEvent(int maxSimultaneousActions)
+        {
+            if (maxSimultaneousActions < 1)
+                maxSimultaneousActions = 1;
+
+            MaxSimultaneousThreads = maxSimultaneousActions;
+            MaxSimultaneousThreadsIgnoreTimeout = int.MaxValue;
+        }
+
+        /// <summary>
+        /// Constructs AsyncResetEvent with the MaxSimultaneousActions allowed.
+        /// </summary>
+        /// <param name="maxSimultaneousActions">The number of actions allowed to be invoked simultaneously. A value less than 1 is defaulted to 1.</param>
+        /// <param name="maxSimultaneousThreadsIgnoreTimeout">The max numer of actions allowed to run ignoring timeout. A value less than maxSimultaneousActions is defaulted to maxSimultaneousActions</param>
+        public AsyncResetEvent(int maxSimultaneousActions, int maxSimultaneousThreadsIgnoreTimeout)
+        {
+            if (maxSimultaneousActions < 1)
+                maxSimultaneousActions = 1;
+
+            MaxSimultaneousThreads = maxSimultaneousActions;
+
+            if (maxSimultaneousThreadsIgnoreTimeout < maxSimultaneousActions)
+                maxSimultaneousThreadsIgnoreTimeout = maxSimultaneousActions;
+
+            MaxSimultaneousThreadsIgnoreTimeout = maxSimultaneousThreadsIgnoreTimeout;
+        }
+
+        /// <summary>
+        /// Forces the current thread to stop until an action has completed.
+        /// If the number of threads reaching WaitAction is less than MaxSimultaneousActions then they will be immediately Invoked.
+        /// </summary>
+        /// <param name="action">The action to Invoke once waiting is complete.</param>
+        /// <param name="timeout">The time in milliseconds the action will wait before it is forced to Invoke.</param>
+        public void WaitAction(Action action, int? timeout = null)
+        {
+            threadCounter++;
+            autoResetEvent.Reset();
+
+            if (threadCounter > MaxSimultaneousThreads)
+                autoResetEvent.WaitOne(threadCounter <= MaxSimultaneousThreadsIgnoreTimeout ? timeout ?? int.MaxValue : int.MaxValue);
+
+            new Thread(() =>
+            {
+                action?.Invoke();
+                threadCounter--;
+                autoResetEvent.Set();
+            }).Start();
+        }
+    }
+}
--- NAsyncThreads/Program.cs
+++ NAsyncThreads/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using static System.Console;
 
 namespace NAsyncThreads
 {
     class Program
     {
+        static AsyncResetEvent asyncResetEvent = new AsyncResetEvent(4);
+        private const int sleepTime = 2000;
+        private const int timeout = 1000;
+
         static void Main(string[] args)
         {
+            var random = new Random();
+
+            for (int i = 0; i < 200; i++)
+                asyncResetEvent.WaitAction(() =>
+                {
+                    WriteLine($"Complete");
+                    Thread.Sleep(random.Next(0, sleepTime));
+                }, timeout);
+
+            WriteLine("End");
+            Read();
         }
-    }
+    }   
 }
